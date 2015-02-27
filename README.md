817c3d01fb72c2c1101356a337f14052d312faa1
Ali Sarac <alisarac@gmail.com>
2015-02-27 22:30:48 +0200

    missing api functions are added

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # clj-gw2
 
-A Clojure library to use Guild Wars 2 api.
-
-More info about [Guild Wars 2 API](http://wiki.guildwars2.com/wiki/API).
+A Clojure wrapper for [Guild Wars 2 API](http://wiki.guildwars2.com/wiki/API).
 
 ## Usage
 
 Guild Wars 2 API
 
 **/items**
-
 **/items/28445**
+**/items/12452**
+
+**/recipes/search?input=46731**
+**/event_details?event_id=EED8A79F-B374-4AE6-BA6F-B7B98D9D7142**
 
 in clj-gw2
 
 **(items)**
+**(items 28445 12452)** ;; in one call 2 items can be retrieved (2 async http calls)
 
-**(item 28445)**
-
-Please note that api implementation has not finished yet.
+**(recipes-search-input 46731)**
+**(event-details-event-id  "EED8A79F-B374-4AE6-BA6F-B7B98D9D7142")**
 
 ## License
 
--- src/clj_gw2/api.clj
+++ src/clj_gw2/api.clj
@@ -5,53 +5,115 @@
             [clojure.string :as s]
             [clojure.core.memoize :as memo]))
 
-(def api-root (or (env :api-root) "https://api.guildwars2.com/v2/"))
-(def endpoints #{:items :recipes [:recipes :search] :skins :continents 
-                 :floors :maps [:commerce :listings] [:commerce :exchange]
-                 [:commerce :prices] :build :colors :files :quaggans :worlds})
+(def api-root-v1 (or (env :api-root-v1) "https://api.guildwars2.com/v1/"))
+(def api-root-v2 (or (env :api-root-v2) "https://api.guildwars2.com/v2/"))
 
-(defn api-request 
-  "General api request"
-  [params f]
-  (http/request params f))
-
-(defn api-get 
+(defn api-get
   "Get subject"
-  [subject f]
-  (api-request {:url (str api-root subject) :method :get} f))
-
-(defn api-post 
-  "Create subject with given parameters, this function will not be used"
-  [subject params f]
-  (api-request (conj params {:url (str api-root subject) :method :post}) f))
-
-(defn parse-result 
-  "Default result parser"
-  [{:keys [status headers body error]}]
-  (if error
-    error
-    (json/parse-string body true)))
+  [api-root subject]
+  (http/get (str api-root subject)))
 
 (def api-get-cached-1m (memo/ttl api-get {} :ttl/threshold 60000))
 (def api-get-cached-full (memo/memo api-get))
 
-;; items
-(defn items []
-  @(api-get-cached-1m "items"  parse-result))
+(defn api-get-one [api-root subject]
+  (let [{:keys [body error] :as r} @(api-get-cached-full api-root subject)]
+    (when-not error (json/parse-string body true))))
 
-(defn item [id]
-  @(api-get-cached-full (str "items/" id) parse-result))
+(defn api-get-all [api-root subject ids]
+  (let [futures (doall (map #(api-get-cached-full api-root (str subject "/" %)) ids))]
+    (for [resp futures
+          :let [{:keys [body error] :as r} @resp]
+          :when (not error)]
+      (json/parse-string body true))))
 
-;; recipes
-(defn recipes []
-  @(api-get-cached-1m "recipes"  parse-result))
+(defn api-fn-gen 
+  "General api function"
+  [api-root subject]
+  (fn
+    ([] (api-get-one api-root subject))
+    ([& ids] (api-get-all api-root subject ids))))
 
-(defn recipe [id]
-  @(api-get-cached-full (str "recipes/" id) parse-result))
+(defn api-fn
+  "General api function for v2 API"
+  [subject] 
+  (api-fn-gen api-root-v2 subject))
 
-(defn recipe-search-input [input]
-  @(api-get-cached-1m (str "recipes/search?" "input=" input) parse-result))
+(defn api-v1-fn
+  "General api function for v2 API"
+  [subject] 
+  (api-fn-gen api-root-v1 subject))
 
-(defn recipe-search-output [output]
-  @(api-get-cached-1m (str "recipes/search?" "output=" output) parse-result))
 
+;; v2 api endpoints
+
+(def items (api-fn "items"))
+
+
+(def recipes (api-fn "recipes"))
+
+(defn recipes-search-input [input]
+  (api-get-one api-root-v2 (str "recipes/search?" "input=" input)))
+
+(defn recipes-search-output [output]
+  (api-get-one api-root-v2 (str "recipes/search?" "output=" output)))
+
+
+(def continents (api-fn "continents"))
+
+;; floors are disabled in v2
+(def floors (api-fn "floors"))
+
+(def maps (api-fn "maps"))
+
+(def skins (api-fn "skins"))
+
+(def commerce-listings (api-fn "commerce/listings"))
+
+;; need to be more modular, quantity=<quantity> is required on sub calls
+(def commerce-exchange (api-fn "commerce/exchange"))
+
+(def commerce-prices (api-fn "commerce/prices"))
+
+(def build (api-fn "build"))
+
+(def colors (api-fn "colors"))
+
+(def files (api-fn "files"))
+
+(def quaggans (api-fn "quaggans"))
+
+(def worlds (api-fn "worlds"))
+
+
+;; v1 endpoints
+
+(def event-names (api-v1-fn "event_names"))
+
+(def map-names (api-v1-fn "map_names"))
+
+(def world-names (api-v1-fn "world_names"))
+
+(def event-details (api-v1-fn "event_details"))
+
+(defn event-details-event-id [id]
+  (api-get-one api-root-v1 (str "event_details?event_id=" id)))
+
+(defn guild-details-guild-id [id]
+  (api-get-one api-root-v1 (str "guild_details?guild_id=" id)))
+
+(defn guild-details-guild-name [id]
+  (api-get-one api-root-v1 (str "guild_details?guild_name=" id)))
+
+(defn map-floor-continent-id-floor [cid floor]
+  (api-get-one api-root-v1 (str "map_floor?continent_id=" cid "&floor=" floor)))
+
+(def wvw-matches (api-v1-fn "wvw/matches"))
+
+(defn wvw-match-details-match-id [id]
+  (api-get-one api-root-v1 (str "wvw/match_details?match_id=" id)))
+
+(def wvw-objective-names (api-v1-fn "wvw/objective_names"))
+
+(defn render [signature file-id format]
+  (str "https://render.guildwars2.com/file/" signature "/" file-id "." format))
